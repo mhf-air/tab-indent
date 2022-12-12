3a133ddb4011a7013b5bb1172845bf4ff1866f2b
mhf <mhfcbz@163.com>
2022-12-12 14:26:40 +0800

    s:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{value_parser, Arg, ArgAction, Command};
 use std::fs;
 use std::path::PathBuf;
 
@@ -7,35 +7,36 @@ fn main() {
     let input_arg = Arg::new("input")
         .long("input")
         .value_name("FILE")
-        .about("Set the input file");
+        .help("Set the input file");
     #[cfg(not(debug_assertions))]
     let input_arg = input_arg.required(true);
     #[cfg(debug_assertions)]
     let input_arg = input_arg.default_value("./src/main.rs");
 
-    let matches = App::new("tab-indent")
+    let matches = Command::new("tab-indent")
         .version("1.0.0")
         .author("Lisper")
-        .about("transform line-leading spaces into tabs")
+        .about("transform line-leading spaces into tabs, and remove line-trailing spaces")
         .arg(input_arg)
         .arg(
             Arg::new("tab-width")
                 .long("tab-width")
-                .takes_value(true)
                 .value_name("TAB-WIDTH")
                 .default_value("4")
-                .about("Set the tab width"),
+                .value_parser(value_parser!(u8))
+                .help("Set the tab width"),
         )
         .arg(
             Arg::new("inplace")
                 .long("inplace")
-                .about("Modify the file inplace"),
+                .action(ArgAction::SetTrue)
+                .help("Modify the file inplace"),
         )
         .get_matches();
 
-    let input = matches.value_of("input").unwrap();
-    let tab_width: u8 = matches.value_of_t("tab-width").unwrap();
-    let inplace = matches.is_present("inplace");
+    let input = matches.get_one::<String>("input").unwrap();
+    let tab_width = *matches.get_one::<u8>("tab-width").unwrap();
+    let inplace = *matches.get_one::<bool>("inplace").unwrap();
 
     // fmt
     let p = fs::canonicalize(PathBuf::from(input)).unwrap();
@@ -49,70 +50,74 @@ fn main() {
 }
 
 fn format_run(text: String, tab_width: u8) -> String {
+    if text.len() == 0 {
+        return text;
+    }
+
     let mut r = String::new();
-    let list: Vec<char> = text.chars().collect();
+    let mut is_first_line = true;
+    for line in text.lines() {
+        if is_first_line {
+            is_first_line = false;
+        } else {
+            r.push('\n');
+        }
 
-    let mut i = 0;
-    let len = list.len();
-    'outer: while i < len {
-        if list[i] == '\n' {
-            r.push(list[i]);
-            i += 1;
-            if i >= len {
-                break;
-            }
+        if line.is_empty() {
+            continue;
+        }
+        let mut chars = line.chars().peekable();
 
-            // skip tabs
-            while list[i] == '\t' {
-                i += 1;
-                r.push('\t');
-            }
+        let mut leading_tabs = 0;
+        while let Some('\t') = chars.peek() {
+            chars.next();
+            leading_tabs += 1;
+        }
 
-            // spaces to tabs
-            if list[i] == ' ' {
-                let mut space_count = 1;
-                i += 1;
-                while list[i] == ' ' {
-                    space_count += 1;
-                    i += 1;
-                }
-                let tabs = space_count / tab_width;
-                for _ in 0..tabs {
-                    r.push('\t');
-                }
-                let spaces = space_count % tab_width;
-                for _ in 0..spaces {
-                    r.push(' ');
-                }
-            }
+        // space to tabs
+        let mut space_count = 0;
+        while let Some(' ') = chars.peek() {
+            chars.next();
+            space_count += 1;
+        }
+        if chars.peek().is_none() {
+            // all tabs or spaces in this line, remove it
             continue;
         }
 
-        // remove line-trailling spaces
-        if list[i] == ' ' {
-            let mut space_count = 1;
-            let mut a = i + 1;
-            while a < len {
-                match list[a] {
-                    ' ' => {
-                        space_count += 1;
-                    }
-                    '\n' => {
-                        i += space_count;
-                        continue 'outer;
-                    }
-                    _ => break,
-                }
-                a += 1;
-            }
-            if a >= len {
-                break;
-            }
+        leading_tabs += space_count / tab_width;
+        for _ in 0..leading_tabs {
+            r.push('\t');
+        }
+        let spaces = space_count % tab_width;
+        for _ in 0..spaces {
+            r.push(' ');
         }
 
-        r.push(list[i]);
-        i += 1;
+        let mut space_count = 0;
+        loop {
+            match chars.peek() {
+                Some(' ') => {
+                    space_count += 1;
+                    chars.next();
+                }
+                None => {
+                    break;
+                }
+                Some(&ch) => {
+                    for _ in 0..space_count {
+                        r.push(' ');
+                    }
+                    r.push(ch);
+                    space_count = 0;
+                    chars.next();
+                }
+            }
+        }
     }
 
+    if text.len() > 1 && text.as_bytes()[text.len() - 2] == '\n' as u8 {
+        r.push('\n');
+    }
     r
 }
